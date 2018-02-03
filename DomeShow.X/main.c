f3ff35976ca9b4a4df8eebef356779e4096650cd
Ian Smith <smit7356@umn.edu>
2018-02-03 13:08:07 -0600

    Added OC back because I'm a fool. Allows testing of signal

--- DomeShow.X/main.c
+++ DomeShow.X/main.c
@@ -80,9 +80,80 @@ int setup(void) {
     U1MODEbits.STSEL = 1;           // 2 stop bits
     U1STAbits.UTXEN = 0;            // Disable transmission
     U1STAbits.URXISEL = 2;          // TODO: Double-check this!!
-            
+    
+    // Timer Setup
+    T1CONbits.TCS = 0;              // Use 1/2 Fosc (16 MHz)
+    T1CONbits.TCKPS = 0b00;         // 1:1 with system clock
+    TMR1 = 0;
+    PR1 = 256;
+    T1CONbits.TON = 1;
+    
+    T2CONbits.T32 = 0;
+    T2CONbits.TCS = 0;              // Use 1/2 Fosc (16 MHz)
+    T2CONbits.TCKPS = 0b00;         // 1/8 system clock
+    TMR2 = 0;
+    PR2 = 65535;
+    _T2IE = 1;
+    T2CONbits.TON = 1;
+    
+    // OC Setup
+    __builtin_write_OSCCONL(OSCCON & 0xbf); // unlock PPS
+    RPOR5bits.RP11R = 18;           // OC1
+    RPOR6bits.RP12R = 19;           // OC2
+    RPOR6bits.RP13R = 20;           // OC3
+    RPOR7bits.RP14R = 21;           // OC4
+    //RPOR7bits.RP15R = 22;           // OC5
+    __builtin_write_OSCCONL(OSCCON | 0x40); // lock PPS
+    
+    OC1CON1 = 0;
+    OC1CON1bits.OCTSEL = 4;         // Use Timer1
+    OC1CON1bits.OCM = 0b110;          // Edge-aligned PWM
+    OC1CON2bits.SYNCSEL = 0b01011;
+    OC1CON2 = 0x0000;
+    //OCxCON2bits.OCINV lets you invert the output. 
+    
+    OC2CON1 = 0x1006;
+    OC2CON2 = 0x0000;
+    OC3CON1 = 0x1006;
+    OC3CON2 = 0x0000;
+    OC4CON1 = 0x1006;
+    OC4CON2 = 0x0000;
+    //OC5CON1 = 0x1006;
+    //OC5CON2 = 0;
+    OC2CON2bits.SYNCSEL = 0b01011;
+    OC3CON2bits.SYNCSEL = 0b01011;
+    OC4CON2bits.SYNCSEL = 0b01011;
+    
+    OC1RS = 255;
+    OC2RS = 255;
+    OC3RS = 255;
+    OC4RS = 255;
+    //OC5RS = 255;
+    
     return 0;
 }
+
+void __attribute__((__interrupt__, __auto_psv__)) _T2Interrupt() {
+    _T2IF = 0;
+    
+    /*
+    tempLevel += 5;
+    OC4R = tempLevel;
+    if(tempLevel == 255) {
+        tempLevel = 0;
+    }
+    */
+    // Write to OCRx registers
+    
+    OC1R = channelValues[0];
+    OC2R = channelValues[1];
+    OC3R = channelValues[2];
+    OC4R = channelValues[3];
+    //OC5R = levels[4];
+    asm("btg LATB, #15");
+    
+}
+
 
 void __attribute__((__interrupt__, __auto_psv__)) _U1RXInterrupt() {
     char temp;
